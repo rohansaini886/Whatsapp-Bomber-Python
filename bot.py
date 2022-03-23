61f15442cd89a71f7924fb844352185ab2d55253
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2022-03-23 11:42:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,10 @@ print("____START____")
 print("Now open Whatsapp web and Click on person to send message")
 time.sleep(30)
 msg = input("Enter Your message :- ")
-run = int(input("Enter How many times you want to message :- :)
+run = int(input("Enter How many times you want to message :- "))
 counter = 1
+print("Now Open Whatsapp Web And Not change The screen")
+time.sleep(5)
 print("Now Just Sit Back")
 for i in range(run):
     pt.write(msg)
